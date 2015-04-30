a12710c6c96a1100530647d8f62f4c4e929c8af3
Dwight Naylor <dwight.naylor@sbcglobal.net>
2015-04-30 19:07:55 -0400

    spawning commit

---
+++ src/helpers/ImageHelper.java
@@ -0,0 +1,134 @@
+package helpers;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ImageHelper {
+	static Map<String, BufferedImage> imgMap = new HashMap<String, BufferedImage>();
+
+	public static BufferedImage load(String fileName) {
+		BufferedImage img = null;
+		if (imgMap.containsKey(fileName)) {
+			img = imgMap.get(fileName);
+		} else {
+			try {
+				InputStream resource = ImageHelper.class.getClassLoader().getResourceAsStream("img/" + fileName);
+				if (resource == null) {
+					return null;
+				}
+				img = ImageIO.read(resource);
+
+				imgMap.put(fileName, img);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return img;
+	}
+
+	public static JFrame showImage(BufferedImage img) {
+		JFrame frame = new JFrame();
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+		frame.pack();
+		frame.setVisible(true);
+		return frame;
+	}
+
+	public static void showImage(BufferedImage img, JFrame frame) {
+		frame.remove(0);
+		frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+		frame.pack();
+		frame.setVisible(true);
+		// ((JLabel) frame.getContentPane().getComponent(0)).setIcon(new ImageIcon(img));
+		// ((JLabel) frame.getContentPane().getComponent(0)).repaint();
+	}
+
+	public static Point find(BufferedImage image, BufferedImage subImage) {
+		return find(image, subImage, 90);
+	}
+
+	public static Point find(BufferedImage image, BufferedImage subImage, int colorEpsilon) {
+		for (int x = 0; x < image.getWidth() - subImage.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight() - subImage.getHeight(); y++) {
+				boolean matches = true;
+				for (int sx = 0; sx < subImage.getWidth() && matches; sx++) {
+					for (int sy = 0; sy < subImage.getHeight() && matches; sy++) {
+						if ((subImage.getRGB(sx, sy) & 0xFF000000) != 0 && colorDist(image.getRGB(x + sx, y + sy), subImage.getRGB(sx, sy)) > colorEpsilon) {
+							matches = false;
+						}
+					}
+				}
+				if (matches) {
+					return new Point(x, y);
+				}
+			}
+		}
+		return null;
+	}
+
+	public static BufferedImage getClosed(BufferedImage image, int closeRadius) {
+		BufferedImage ret = deepCopy(image);
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				if (image.getRGB(x, y) == 0xFFFFFFFF) {
+					for (int sx = Math.max(0, x - closeRadius); sx <= Math.min(image.getWidth() - 1, x + closeRadius + 1); sx++) {
+						for (int sy = Math.max(0, y - closeRadius); sy <= Math.min(image.getHeight() - 1, y + closeRadius + 1); sy++) {
+							if (image.getRGB(sx, sy) == 0xFF000000) {
+								ret.setRGB(x, y, 0xFF000000);
+							}
+						}
+					}
+				}
+			}
+		}
+		return ret;
+	}
+
+	private interface ImageFunction {
+		public int getColor(BufferedImage image, Point pixel);
+	}
+
+	public static void applyFunction(BufferedImage image, ImageFunction f) {
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				image.setRGB(x, y, f.getColor(image, new Point(x, y)));
+			}
+		}
+	}
+
+	public static BufferedImage deepCopy(BufferedImage bi) {
+		ColorModel cm = bi.getColorModel();
+		boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+		WritableRaster raster = bi.copyData(null);
+		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+	}
+
+	public static int colorDist(int cv1, int cv2) {
+		Color c1 = new Color(cv1);
+		Color c2 = new Color(cv2);
+		return Math.abs(c1.getRed() - c2.getRed()) + Math.abs(c1.getGreen() - c2.getGreen()) + Math.abs(c1.getBlue() - c2.getBlue());
+	}
+
+	public static void applyFunction(BufferedImage image, Function<Integer, Integer> f) {
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				image.setRGB(x, y, f.apply(image.getRGB(x, y)));
+			}
+		}
+	}
+}
---
+++ src/main/ScreenReader.java
@@ -0,0 +1,90 @@
+package main;
+
+import helpers.ImageHelper;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JRootPane;
+
+public class ScreenReader {
+	Robot r;
+	Rectangle screenRectangle;
+
+	/**
+	 * The change in screen location since the last frame
+	 */
+	Point change = new Point(0, 0);
+	BufferedImage oldImage;
+
+	public ScreenReader() {
+		try {
+			r = new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public BufferedImage getWholeScreen() {
+		return r.createScreenCapture(new Rectangle(0, 0, Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height));
+	}
+
+	public void isolateGameScreen() {
+		BufferedImage screen = getWholeScreen();
+		Point upperLeftRough = ImageHelper.find(screen, ImageHelper.load("windowUpperLeft.png"));
+		if (upperLeftRough == null) {
+			System.err.println("Unable to find game window.");
+			System.exit(0);
+		}
+		Point upperLeft = new Point(upperLeftRough.x - 22, upperLeftRough.y + 20);
+		screenRectangle = new Rectangle(upperLeft.x, upperLeft.y, 600, 600);
+		// ImageHelper.showImage(r.createScreenCapture(screenRectangle));
+	}
+
+	private void processNewImage(BufferedImage newImage) {
+		if (oldImage != null) {
+			
+		}
+		oldImage = newImage;
+	}
+
+	public void showDisplay() {
+		JFrame frame = new JFrame();
+		frame.setContentPane(new JRootPane() {
+			@Override
+			public void paint(Graphics g) {
+				super.paint(g);
+				BufferedImage screen = r.createScreenCapture(screenRectangle);
+				processNewImage(screen);
+				//Adjusting the output image...
+				ImageHelper.applyFunction(screen, (Integer i) -> ImageHelper.colorDist(i, 0xFF000000) < 200 ? 0xFF000000 : 0xFFFFFFFF);
+//				screen = ImageHelper.getClosed(screen, 1);
+				g.drawImage(screen, 0, 0, null);
+			}
+		});
+		frame.getContentPane().setPreferredSize(new Dimension(screenRectangle.width, screenRectangle.height));
+		frame.pack();
+		frame.setVisible(true);
+		while (true) {
+			frame.repaint();
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		ScreenReader s = new ScreenReader();
+		s.isolateGameScreen();
+		s.showDisplay();
+	}
+}
